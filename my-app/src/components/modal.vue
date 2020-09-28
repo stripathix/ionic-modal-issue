92be4ad87239304d465ac43cbde07f174f1903d5
Shashwat Tripathi <shashwattripathi@Shashwats-MacBook-Pro.local>
2020-09-28 23:51:23 +0530

    ion-modal issue

---
+++ my-app/src/components/modal.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>{{ title }}</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content class="ion-padding">
+      {{ content }}
+    </ion-content>
+  </div>
+</template>
+
+<script>
+import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/vue';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Modal',
+  props: {
+    title: { type: String, default: 'Super Modal' },
+  },
+  data() {
+    return {
+      content: 'Content',
+    }
+  },
+  components: { IonContent, IonHeader, IonTitle, IonToolbar }
+});
+</script>
---
+++ my-app/src/views/Tab1.vue
@@ -0,0 +1,46 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Tab 1</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Tab 1</ion-title>
+        </ion-toolbar>
+      </ion-header>
+    <ion-button @click="openmodal">open modal</ion-button>
+      <ExploreContainer name="Tab 1 page" />
+    </ion-content>
+  </ion-page>
+</template>
+
+<script>
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonButton, modalController } from '@ionic/vue';
+import ExploreContainer from '@/components/ExploreContainer.vue';
+import Modal from '@/components/modal.vue';
+export default  {
+  name: 'Tab1',
+  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonButton },
+  methods: {
+    async openmodal() {
+          const modal = await modalController
+            .create({
+              component: Modal,
+              cssClass: 'my-custom-class',
+              componentProps: {
+                data: {
+                  content: 'New Content',
+                },
+                propsData: {
+                  title: 'New title',
+                },
+              },
+            })
+          return modal.present();
+        },
+  }
+}
+</script>
